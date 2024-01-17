8b5a4661edbe175ae9beb23dc6030652dde6c6e2
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-01-17 12:07:27 -0300

    add roteamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import Home from './paginas/home/Home';
 import './App.css';
+import Footer from './components/footer/Footer';
+import Login from './components/login/Login';
+import Navbar from './components/navBar/NavBar';
+import Home from './paginas/home/Home';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
-   <Home />
-  );
+    <>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+);
 }
-
 export default App;
