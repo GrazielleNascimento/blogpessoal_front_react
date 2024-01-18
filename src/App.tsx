0471679e3c31785564c801b93e8d671d9e1ef0ae
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-01-18 14:17:22 -0300

    feat(context, loader, style): integrar contexto de autenticação e carregador, atualizar formatos de ativos
    - Integrar o envoltório de contexto `AuthProvider` em `App.tsx` para gerenciamento do estado de autenticação
    - Adicionar o pacote `react-loader-spinner` para indicadores de carregamento
    - Substituir ativos no formato `.jpg` por `.png` para consistência
    - Implementar funções de registro de usuário em `Cadastro.tsx`
    - Atualizar os componentes `Home.tsx` e `Login.tsx` para utilizar o novo contexto de autenticação
    - Modificar referências de imagens de fundo de `.jpg` para `.png` nos arquivos CSS relacionados
    
    A aplicação agora utiliza `AuthProvider` para o gerenciamento global do estado de autenticação, introduz um indicador de carregamento para uma melhor experiência do usuário durante operações assíncronas, e os formatos de ativos de imagem foram padronizados em toda a aplicação. A página de registro (`Cadastro.tsx`) foi aprimorada com lógica baseada em estado e conectada aos serviços de autenticação. O fluxo de login agora reflete as alterações no contexto de autenticação conforme necessário.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
-import './App.css';
-
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './pages/login/Login';
-import Cadastro from './pages/cadastro/Cadastro';
-import Home from './pages/home/Home';
-
+import Footer from "./components/footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Home from "./pages/home/Home";
+import Navbar from "./components/navBar/NavBar";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <>
-   
-        <BrowserRouter>
-          <Navbar />
-          <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Cadastro />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-  
+     <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
