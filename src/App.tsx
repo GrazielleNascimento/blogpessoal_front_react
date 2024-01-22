c0c59e00f9de54d1bca61ef120bb869b816930b8
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-01-22 10:43:34 -0300

    mude o idioma para português do Brasil e atualize o título > > Adicionadas novas funcionalidades e rotas refatoradas na aplicação. Excluído CSS não utilizado, alterado o atributo da linguagem HTML para 'pt-BR', atualizado o título no index.html, adicionado roteamento para gerenciamento de temas (criar, listar, atualizar, excluir) e implementar a funcionalidade de logout. > > - Defina o idioma em index.html para 'pt-BR' e altere o título do documento para "Blog Pessoal". > - Removido o arquivo App.css porque não era mais necessário. > - Atualizado o App.tsx para incluir novas rotas e removido as antigas. > - NavBar.tsx refatorado com renderização condicional baseada no contexto do usuário e incluído novos links para gerenciamento de temas e logout. > - Criei novos componentes para gerenciamento de temas: ListaTemas, CardTemas, DeletarTema, FormularioTema. > - Service.ts atualizado para incluir novos métodos de serviço HTTP: buscar, cadastrar, atualizar, deletar. > > O commit fornece melhor suporte ao idioma português brasileiro, limpa a estrutura do projeto, melhora a legibilidade e amplia a funcionalidade do aplicativo com recursos de gerenciamento de temas. A remoção do App.css sugere uma mudança para estilos inline ou uma abordagem diferente de estilo. Adicionar recursos de logout aumenta a segurança do aplicativo e o gerenciamento de usuários." error: pathspec 'Pessoal. - Removido o arquivo App.css porque não era mais necessário. - Atualizado o App.tsx para incluir novas rotas e removido as antigas. - NavBar.tsx refatorado com renderização condicional baseada no contexto do usuário e incluído novos links para gerenciamento de temas e logout. - Criei novos componentes para gerenciamento de temas: ListaTemas, CardTemas, DeletarTema, FormularioTema. - Service.ts atualizado para incluir novos métodos de serviço HTTP: buscar, cadastrar, atualizar, deletar.
    O commit fornece melhor suporte ao idioma português brasileiro, limpa a estrutura do projeto, melhora a legibilidade e amplia a funcionalidade do aplicativo com recursos de gerenciamento de temas. A remoção do App.css sugere uma mudança para estilos
    inline ou uma abordagem diferente de estilo. Adicionar recursos de logout aumenta a segurança do aplicativo e o gerenciamento de usuários.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,41 @@
-import Footer from "./components/footer/Footer";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./pages/login/Login";
-import Cadastro from "./pages/cadastro/Cadastro";
-import Home from "./pages/home/Home";
-import Navbar from "./components/navBar/NavBar";
-import { AuthProvider } from "./contexts/AuthContext";
+
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/home/Home';
+import { AuthProvider } from './contexts/AuthContext';
+
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
+import ListaTemas from './components/temas/ListaTemas.tsx/ListaTemas';
+import Navbar from './components/navBar/NavBar';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+
 
 function App() {
   return (
     <>
-     <AuthProvider>
-      <BrowserRouter>
-        <Navbar />
-        <div className="min-h-[80vh]">
-          <Routes>
-            <Route path="/" element={<Cadastro />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/cadastro" element={<Cadastro />} />
-            <Route path="/home" element={<Home />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
-      </AuthProvider>
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              
+
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+        </AuthProvider>
     </>
   );
 }
 export default App;
-//Este é um exemplo de um componente React chamado App que é o componente raiz do aplicativo. Ele usa o React Router DOM para criar rotas para diferentes páginas em nosso aplicativo, e também usa um contexto chamado UserContext, que foi definido em outro lugar do aplicativo.
