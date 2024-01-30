619b1d6b3dfcd03a8d0f8a1efec9a884e7e0e912
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-01-30 20:23:38 -0300

    feat(home): update home page layout and assets
    Update the home page to include a new background image, adjust styling, and integrate the Postings List component. Changes include:
    - Replace `cadastro.png` with `nave.jfif` for the logo.
    - Remove `Home.css` file and inline styles within `Home.tsx`.
    - Add `ListaPostagens` component to display postings list.
    - Enhance button style to be more prominent and add bounce animation.
    - Implement a full-cover background with the new `home.jfif` asset.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,34 +1,34 @@
 
-import homeLogo from '../../assets/cadastro.png'
+import homeLogo from '../../assets/nave.jfif'
+import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
 import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
-import './Home.css';
+
+
 
 function Home() {
     return (
         <>
-        <div className="bg-indigo-900 flex justify-center">
+        <div className="bg-[url('./assets/home.jfif')] bg-cover p-20 flex justify-center">
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
               <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
-              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
+              <p className='text-xl font-semibold'>Expresse aqui seus Pensamentos e Opiniões</p>
   
               <div className="flex justify-around gap-4">
               <ModalPostagem />
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
-              </div>
+              <button className=' font-bold rounded bg-white text-blue-800 py-2 px-4 animate-bounce'>Ver postagens</button>
+            </div>
             </div>
   
-            <div className="flex justify-center ">
-              <img src={homeLogo} alt="" className='w-2/3' />
+            <div className="flex justify-center p-8 ">
+              <img src={homeLogo} alt="" className='w-2/3  rounded-[2000px] shadow-2xl' />
       
             </div>
           </div>
         </div>
-        
+        <ListaPostagens />
       </>
     );
 }
 
 export default Home;
-
-//Em resumo o componente home apresenta a informação de nome do UserContext e apresenta um botão de voltar que retorna a aplicação para a pagina login
