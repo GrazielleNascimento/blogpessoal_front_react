d7702ea36524f6aaa88947b554910ad5a1da1ee3
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-01-30 20:24:17 -0300

    style(card-postagem): add shadow to card and update header background color
    This commit improves the visual appearance of the `CardPostagem` component by adding a shadow effect, making it stand out on the page (`shadow-lg`). Additionally, the background color of the header has been changed from a generic indigo class to a specific hexadecimal color value (`#AB34CB`), likely for brand consistency or design preference. These changes enhance the user interface without affecting functionality.

--- src/components/postagens/cardPostagem/CardPostagem.tsx
+++ src/components/postagens/cardPostagem/CardPostagem.tsx
@@ -7,9 +7,9 @@ interface CardPostagemProps {
 
 function CardPostagem({post}: CardPostagemProps) {
   return (
-    <div className='border-slate-900 border flex flex-col rounded overflow-hidden justify-between'>
+    <div className='shadow-lg border flex flex-col rounded overflow-hidden justify-between'>
       <div>
-        <div className="flex w-full bg-indigo-400 py-2 px-4 items-center gap-4">
+        <div className="flex w-full bg-[#AB34CB] py-2 px-4 items-center gap-4">
           <img src={post.usuario?.foto} className='h-12 rounded-full' alt="" />
           <h3 className='text-lg font-bold text-center uppercase '>{post.usuario?.nome}</h3>
         </div>
